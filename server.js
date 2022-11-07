1f54c4e2c807a0fbfd76eb80e1d8e8debdf15442
irfanonk <onk.irfan@gmail.com>
2022-11-07 10:57:32 +0300

    add tags route

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const db = require("./db.json");
 
-server.use(middlewares)
+const PORT = 8001;
+
+server.use(middlewares);
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.get("/tags", function (req, res) {
+  const allTags = [];
+  const items = db.items;
+  items.forEach((item) => {
+    item.tags.forEach((tag) => {
+      if (allTags.includes(tag)) return;
+      allTags.push(tag);
+    });
+  });
+  return res.jsonp(allTags);
+  var tags = db.find("items").find({ tags }).value();
+  if (tags) {
+    res.jsonp(tags);
+  } else {
+    res.sendStatus(404);
+  }
+});
+server.use(router);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running port: ${PORT}`);
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
